9b0221b0128f61fe4545c53ed64b7539f9cdfbb6
kentauros1 <48154351+kentauros1@users.noreply.github.com>
2021-10-24 02:10:18 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -64,3 +64,37 @@ console.log(mes3);
  */
 const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
 sayHello(); //こんにちはゲストさん
+
+/**
+ * スプレッド構文...
+ */
+//配列の展開
+const arr1 = [1, 2];
+console.log(...arr1); //1 2
+
+const sum = (num1, num2) => console.log(num1 + num2);
+console.log(sum(...arr1)); //3
+
+//まとめる
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1); //1
+console.log(num2); //2
+console.log(arr3); //[3,4,5]
+
+//配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+console.log(arr6); //[10,20]
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7); //[10,20,30,40]
+
+//コピーの問題点
+//参照渡しのため、コピー元の値も変わってしまう
+const arr8 = arr4;
+console.log(arr8); //[10,20]
+arr8[0] = 100;
+console.log(arr4); //[100,20]
