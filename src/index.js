4f788e2d3fd23f8d756d3bf8d76a3ca34dc7e49d
kentauros1 <48154351+kentauros1@users.noreply.github.com>
2021-10-24 01:44:49 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -20,3 +20,22 @@ const val2 = {
 };
 const mes = `I am ${val2.name}. ${val2.age}years old.`;
 console.log(mes);
+
+/**
+ * 3.アロー関数
+ */
+//従来の関数の書き方1
+function func1(str) {
+  return str;
+}
+//従来の関数の書き方2
+const func2 = function (str) {
+  return str;
+};
+
+//アロー関数
+const func3 = (str) => {
+  return str;
+};
+//内部式が一行の場合は{}は省略可能
+const func4 = (num1, num2) => num1 + num2;
